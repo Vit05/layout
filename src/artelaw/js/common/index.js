74fdc39ed9d4f16a3631d165c153698e04ceb32a
Vit05 <viktorpaliy88@gmail.com>
2019-01-25 08:05:35 +0200

    desktop press center v3

--- src/artelaw/js/common/index.js
+++ src/artelaw/js/common/index.js
@@ -4,3 +4,30 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev2',
     },
 });
+
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    var fixedHeader = $('.fixed_header.fade');
+    var fixedScrollHeader = $('.header');
+
+    if (scroll >= 200) {
+        fixedHeader.addClass("in");
+        fixedScrollHeader.addClass("fade");
+    } else {
+        fixedHeader.removeClass("in");
+        fixedScrollHeader.removeClass("fade")
+    }
+});
+
+
+//  MOBILE NAVIGATION
+var mobNavBtn = $('#mobNavBtn');
+
+mobNavBtn.on('click', function () {
+    // $('.main-nav').toggleClass('show')
+    var fixedScrollHeader = $('.header');
+
+    fixedScrollHeader.toggleClass("in")
+    mobNavBtn.children().toggleClass('active')
+})
